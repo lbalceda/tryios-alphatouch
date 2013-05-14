7b7da7d9d299ecb7f8aeaedf5b12c9a6602afff4
lbalceda <lbalceda@gmail.com>
2013-05-14 13:01:21 -0700

    1.3

--- Alphatouch/AppDelegate.m
+++ Alphatouch/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"Application Did Finish Launching");
     // Override point for customization after application launch.
     return YES;
 }
